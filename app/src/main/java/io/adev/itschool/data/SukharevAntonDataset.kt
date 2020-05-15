de3cb3d92478bbd48d3ea673d5375394322d26d8
Anton <sanjaplushka@gmail.com>
2020-05-15 15:13:27 +0500

    Добавил свои продукты

--- app/src/main/java/io/adev/itschool/data/SukharevAntonDataset.kt
+++ app/src/main/java/io/adev/itschool/data/SukharevAntonDataset.kt
@@ -13,7 +13,7 @@ class SukharevAntonDataset: BaseDataset() {
                         name = "Звездная ночь",
                         price = 2000000.0,
                         discountPercent = 10,
-                        description = "Одна из наиболее известных картин нидерландского художника-постимпрессиониста Винсента Ван Гога",
+                        description = "Одна из наиболее известных картин нидерландского художника-постимпрессиониста Винсента Ван Гога. Почему, раз увидев, вы уже ее не забудете? Что за воздушные вихри изображены в небе? Почему звезды такие крупные? И как картина, которую Ван Гог считал неудачной, стала “иконой” для всех экспрессионистов? Картина была написана в тяжёлый период жизни Ван Гога. За полгода до этого совместное проживание с Полем Гогеном закончилось прескверно. Мечта Ван Гога о создании южной мастерской, союза художников-единомышленников, не осуществилась.",
                         imageUrl = "https://cdn.britannica.com/78/43678-050-F4DC8D93/Starry-Night-canvas-Vincent-van-Gogh-New-1889.jpg",
                         attributes = mutableListOf(
                             Product.Attribute(
@@ -89,7 +89,7 @@ class SukharevAntonDataset: BaseDataset() {
                         price = 200000.0,
                         discountPercent = 15,
                         description = "Эта картина, передающая атмосферу богемного Парижа начала 20 века, была заказана Пикассо хозяином кабаре Лапин Агиль (Проворный кролик) на Монмартре, и провисела там в течение нескольких лет. Хозяин кабаре изображен на заднем плане картины, с гитарой, а в образе Арлекина у барной стойки Пикассо изобразил самого себя",
-                        imageUrl = "http://picassolive.ru/wp-content/uploads/2013/02/Pablo-Picasso_Au-Lapin-Agile_1905.jpg",
+                        imageUrl = "https://paintingplanet.ru/pictures/image311_3.jpg",
                         attributes = mutableListOf(
                             Product.Attribute(
                                 name = "Год создания",
@@ -103,7 +103,7 @@ class SukharevAntonDataset: BaseDataset() {
                         price = 300000.0,
                         discountPercent = 20,
                         description = "Она приходила позировать в мастерскую Пикассо в общежитие Бато-Лавуар десятки раз, портрет не давался, Пикассо непрерывно переписывал голову, сначала в профиль, потом анфас, потом в три четверти. Наконец, он воскликнул - Всё, я больше уже не вижу вас! - и отставив работу, уехал с Фернандой в путешествие в Испанию. Вернувшись, он закончил работу",
-                        imageUrl = "http://picassolive.ru/wp-content/uploads/2012/07/Pablo-Picasso_Portrait-de-Gertrude-Stein_1905-1906.jpg",
+                        imageUrl = "https://lh3.googleusercontent.com/proxy/GYebCs0Ux7WKprwoRxgEXY4oVAgy_8aRG6WeeQsUYn9wHl-Y3IeNSHzCfmgGU1WZ29lDzj4DU7pPMNu3akd-8pD4IFnN2Jw2VSvwi4hVCu_DvjsBzuowH-rhbkZjCJaCPbjvOeH6AZnZ",
                         attributes = mutableListOf(
                             Product.Attribute(
                                 name = "Год создания",
@@ -121,7 +121,7 @@ class SukharevAntonDataset: BaseDataset() {
                         price = 3000000.0,
                         discountPercent = 15,
                         description = "Автопортрет написан в тот же год, что и картина Авиньонские девицы, и напоминает эскизы к ней. Этот период, для которого характерны огрубление и примитивизация формы, носит название сезанновского",
-                        imageUrl = "http://picassolive.ru/wp-content/uploads/2013/06/Pablo-Picasso_Autoportrait_1907.jpg",
+                        imageUrl = "https://muzei-mira.com/templates/museum/images/paint/avtoportret-picasso-1901+.jpg",
                         attributes = mutableListOf(
                             Product.Attribute(
                                 name = "Год создания",
@@ -135,7 +135,7 @@ class SukharevAntonDataset: BaseDataset() {
                         price = 4000000.0,
                         discountPercent = 12,
                         description = "С 1917 года круг общения Пикассо расширяется, кроме парижской богемы он вращается в театральной среде, оформляя балет Парад для труппы Дягилева",
-                        imageUrl = "http://picassolive.ru/wp-content/uploads/2011/12/Pablo-Picasso_Les-baigneuses_19182.jpg",
+                        imageUrl = "https://muzei-mira.com/templates/museum/images/paint/kupalshici-picasso+.jpg",
                         attributes = mutableListOf(
                             Product.Attribute(
                                 name = "Год создания",
@@ -149,7 +149,7 @@ class SukharevAntonDataset: BaseDataset() {
                         price = 4500000.0,
                         discountPercent = 15,
                         description = "Пикассо попадает в театральную среду, он оформляет спектакли для балета труппы Дягилева, влюбляется в русскую балерину Ольгу Хохлову, в следующем году они поженятся. Ольга хочет, чтобы на портретах она была похожа, она склонна к традиционности, консерватизму и респектабельности",
-                        imageUrl = "http://picassolive.ru/wp-content/uploads/2016/04/Pablo-Picasso-Portrait-d-Olga-dans-un-fauteuil-1917.jpg",
+                        imageUrl = "https://opisanie-kartin.com/pictures/2/image159.jpg",
                         attributes = mutableListOf(
                             Product.Attribute(
                                 name = "Год создания",
@@ -161,7 +161,44 @@ class SukharevAntonDataset: BaseDataset() {
             ),
             Category(
                 name = "Иван Айвазовский",
-                products = mutableListOf()
+                products = mutableListOf(
+                    Product(
+                        id = "11",
+                        name = "Девятый вал",
+                        price = 100000.0,
+                        discountPercent = 10,
+                        description = "Бушующий океан, группа моряков, жаждущих спасения на обломках мачт… И первые утренние лучи солнца, дарящие надежду на лучшее. Айвазовский будто говорит: «Посмотрите, что пришлось пережить этим людям, какое крушение потерпел их корабль». Он рисует отвагу моряков на фоне лучезарных волн и сильного ветра. Картина полна света, тепла и воздуха. Живописец не пожалел ярких красок и в полной мере выразил всю мощь как водной стихии, так и людей.",
+                        imageUrl = "https://s12.stc.all.kpcdn.net/share/i/4/689438/inx960x640.jpg",
+                        attributes = listOf(
+                            Product.Attribute(
+                                name = "Год создания", value = "1850"
+                            )
+                        )
+                    ),
+                    Product(
+                        id = "12",
+                        name = "Море. Коктебель",
+                        price = 1500000.0,
+                        discountPercent = 15,
+                        description = "На ней художник изобразил свою родину, Феодосию, где он провел лучшие годы жизни. Эта работа хранится в картинной галерее Айвазовского в том же курортном городе Украины. Утонченное сочетание оранжевого, розового, лилового цветов дают этой картине неповторимое тепло. Нежное небо, переданное в перламутре заката, переплетается со светлыми волнами. Благодаря этому картина играет, от нее веет жизненной силой, необычной пленительной красотой Черного моря. Здесь Иван Айвазовский достиг подлинного мастерства мариниста",
+                        imageUrl = "https://muzei-mira.com/templates/museum/images/paint/more.-koktebel-ayvazovskiy+.jpg",
+                        attributes = listOf(
+                            Product.Attribute(
+                                name = "Год создания", value = "1847"
+                            ))
+                    ),
+                    Product(
+                        id = "12",
+                        name = "Чесменский бой",
+                        price = 1900000.0,
+                        discountPercent = 15,
+                        description = "Художник показал на полотне одно из самых героических сражений в истории российского флота, происходившего в ночь на 26 июня 1770 года. Как точно он передает то, чего не видел сам, но пережили моряки. Кругом горят и взрываются корабли, вспыхивают мачты, взлетают в воздух их обломки. Алый огонь смешивается с серой водой, как наши, русские, моряки с турецкими. Светлая луна глядит на сражение свысока, как бы предсказывая предстоящую победу над турецким флотом.",
+                        imageUrl = "https://img.gazeta.ru/files3/668/10809668/upload-Chesmabattle-pic905-895x505-27631.jpg",
+                        attributes = listOf(Product.Attribute(
+                            name = "Год создания", value = "1848")
+                        )
+                    )
+                )
             ),
             Category(
                 name = "Иван Шишкин",
